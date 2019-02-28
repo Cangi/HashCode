34b0253b2806e87ff13510150bcf307fb398b6b7
AstryX <robertasdere@gmail.com>
2019-02-28 19:03:45 +0000

    Score calculation added

--- mainscript.py
+++ mainscript.py
@@ -14,3 +14,20 @@ with open('data.txt','r') as f:
         for i in range(int(count)):
             dic[real_line[2 + i]].append({i, t})
             print(dic[t])
+	
+	
+	
+def evaluateScore(slides):
+	totalScore = 0;
+	for i in (range(len(slides))-1):
+		scoreList = []
+		commScore = getIntersection(slides[i],slides[i+1])
+		scoreList.append(commScore)
+		scoreList.append(len(slides[i]) - commScore)
+		scoreList.append(len(slides[i+1]) - commScore)
+		totalScore = totalScore + min(scoreList)
+	return totalScore
+		
+def getIntersection(arr1, arr2): 
+    combined = [value for value in arr1 if value in arr2] 
+    return len(combined) 
