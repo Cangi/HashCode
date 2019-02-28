df7f1451aeaceea3f6fb533f050fa4d3fd88db93
AstryX <robertasdere@gmail.com>
2019-02-28 18:38:42 +0000

    Paulius image read

--- mainscript.py
+++ mainscript.py
@@ -1,3 +1,16 @@
-import pandas as pd
-import numpy as np
-import opencv as ocv
+# f = open('data.txt')
+# arr = f.read().split()
+# n= arr[0]
+# print(n)
+# f.close()
+dic = {}
+with open('data.txt','r') as f:
+    for line in f:
+        real_line = line.split()
+        if len(real_line) == 1:
+            continue
+        t = real_line[0]
+        count = real_line[1]
+        for i in range(int(count)):
+            dic[real_line[2 + i]].append({i, t})
+            print(dic[t])
